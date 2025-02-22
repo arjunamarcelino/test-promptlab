de4f32197d17df77a63581dcbbf5fa3d47c24806
Arjuna Marcelino <arjunamarcelino@gmail.com>
2025-02-22 18:05:27 +0700

    feat(): section connect tools

--- app/_components/home/Connect.js
+++ app/_components/home/Connect.js
@@ -1,16 +1,9 @@
 "use client";
 import { useEffect, useState } from "react";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 export default function Connect() {
-  //   const [isMounted, setIsMounted] = useState(false);
-
-  //   useEffect(() => {
-  //     setIsMounted(true);
-  //   }, []);
-
-  //   if (!isMounted) return null;
-
   const [activeTab, setActiveTab] = useState("Github & Gitlab");
 
   const tabs = [
@@ -23,23 +16,34 @@ export default function Connect() {
   return (
     <section
       id="connect"
-      className="relative w-full min-h-screen flex flex-col items-center justify-center bg-background text-white"
+      className="relative w-full min-h-screen flex flex-col items-center justify-center bg-background text-white px-[60px] mb-[171px]"
     >
-      <h2 className="text-4xl font-semibold text-center">
+      <motion.h2
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        transition={{ delay: 0.5, type: "spring", stiffness: "250" }}
+        className="text-4xl font-semibold text-center"
+      >
         Connect with Your Favorite Tools
-      </h2>
-      <p className="text-lg text-primary-100 mt-2 text-center">
+      </motion.h2>
+      <motion.p
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        transition={{ delay: 0.5, type: "spring", stiffness: "250" }}
+        className="text-lg text-primary-100 mt-4 text-center"
+      >
         Type your prompt. Watch your code come to life.
-      </p>
+      </motion.p>
 
-      {/* Tabs */}
-      <div className="flex space-x-6 mt-8 border-b border-[#1E3A47] pb-4">
+      <div className="flex space-x-6 mt-8 pb-4">
         {tabs.map((tab) => (
           <button
             key={tab}
-            className={`px-5 py-2 rounded-full transition-all duration-200 ${
+            className={`px-5 py-3 rounded-full transition-all duration-200 ${
               activeTab === tab
-                ? "bg-gradient-to-b from-[#00D1FF] to-[#007B9F] text-white"
+                ? "border border-[#5A879D] text-[#BBDCFF]"
                 : "text-gray-400 hover:text-white"
             }`}
             onClick={() => setActiveTab(tab)}
@@ -49,22 +53,19 @@ export default function Connect() {
         ))}
       </div>
 
-      {/* Content Box */}
-      <div className="mt-10 w-full max-w-5xl bg-gradient-to-b from-[#10181E] to-[#0E1F25] rounded-xl p-8 flex flex-col md:flex-row items-center shadow-lg">
-        {/* Text Section */}
+      <div className="min-h-[547px] mt-10 w-full bg-gradient-to-b from-[#10181E] to-[#0E1F25] rounded-xl p-12 flex flex-col md:flex-row items-center shadow-lg">
         <div className="w-full md:w-1/2">
-          <h3 className="text-3xl font-semibold">GitHub & GitLab</h3>
-          <p className="text-gray-400 mt-4">
+          <h3 className="text-5xl font-semibold">GitHub & GitLab</h3>
+          <p className="text-lg text-secondary-200 mt-4 max-w-[490]">
             Push, pull, and manage your repositories with built-in GitHub and
             GitLab integration. Track changes, collaborate with teams, and
             commit updates directly from PromptLab—no extra setup required.
           </p>
         </div>
 
-        {/* Image Section */}
         <div className="w-full md:w-1/2 flex justify-center mt-6 md:mt-0">
           <Image
-            src="/images/github-gitlab.png" // Ganti dengan path gambar yang sesuai
+            src="/images/img-connect-tools.png"
             alt="GitHub & GitLab"
             width={300}
             height={200}
